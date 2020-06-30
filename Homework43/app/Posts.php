a78a5596afd05ed2ea556fffbca10caf8a9c6ce7
Mladenovski Sasho <61666956+mlades@users.noreply.github.com>
2020-06-30 09:44:20 +0200

    HW-43

---
+++ Homework43/app/Posts.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    public function published() {
+        $this->published = true;
+        $this->save();
+    }
+}
