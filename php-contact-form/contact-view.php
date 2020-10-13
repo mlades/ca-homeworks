16484919c32f2a6ff427bf17bac2adfebd64b80a
Mladenovski Sasho <61666956+mlades@users.noreply.github.com>
2020-10-13 13:48:33 +0200

    Update contact-view.php

--- php-contact-form/contact-view.php
+++ php-contact-form/contact-view.php
@@ -48,8 +48,7 @@
         </form>
     </div>
 
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"
-        type="text/javascript"></script>
+    
     <script type="text/javascript">
         function validateContactForm() {
             var valid = true;
